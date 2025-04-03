5f5aeca9bff027ee73a1f07f2c592da898c4694a
Filip Sain <filipsain@Filips-MacBook-Pro.local>
2025-04-02 23:09:19 -0400

    reformating HJM functions

--- src/options_pricing/heath_jarrow_morton.py
+++ src/options_pricing/heath_jarrow_morton.py
@@ -12,7 +12,7 @@ class HeathJarrowMorton():
         self.interest_rate_curve_parameters = {
             "exponential": {
                 "name": "exponential",
-                "parameters": [0.4]
+                "parameters": [0.04]
             },
             "nss": {
                 "name": "nss",
@@ -23,17 +23,19 @@ class HeathJarrowMorton():
         self.discount_curve = []
         self.T = 10
         self.dt = 0.1
+        self.sigma = 0.01
 
-
-    def get_yield_curve(self) -> None:
+    def set_yield_curve(self) -> None:
+        """Populate self.yield_curve."""
+        self.yield_curve = []
         if self.interest_rate_curve == "exponential":
             constant_r = self.interest_rate_curve_parameters["parameters"][0]
             num_timesteps = int(self.T/self.dt)
             self.yield_curve = list(constant_r for _ in range(num_timesteps))
         return
 
-    def get_discount_curve(self) -> None:
-        """From yield curve compute discount curve."""
+    def set_discount_curve(self) -> None:
+        """From self.yield curve compute self.discount_curve."""
         self.discount_curve = []
         self.discount_curve.append(1.0)
         for i in range(len(self.yield_curve)):
@@ -42,53 +44,65 @@ class HeathJarrowMorton():
             )
         return
 
-    def graph_exponential_example(self) -> None:
-        sigma = 0.005
-        P = lambda t: math.exp(-0.04 * t)
-        f0 = lambda t: -(math.log(P(t + .1)) - math.log(P(t))) / 0.1
-        frf = lambda T: (f0(T + .1) - f0(T - .1)) / 0.2 + sigma * sigma * T
-        theta = lambda x: frf
-        hl = ho_lee.HoLee(theta, sigma)
-
-        time_values = [i / 10 for i in range(1, 101)]
-
-        # do a bunch of Ho-Lee simulations
-        number_of_runs = 1000
-        run_results = []
-        M_matrix = []
+    def run_ho_lee_simulations(self, number_of_runs: int, ho_lee: ho_lee, f0: Callable):
+        """Given Ho-Lee """
+        run_results_retval = []
+        M_matrix_retval = []
         for _ in range(number_of_runs):
-            interest_rates = hl.eval_time_T_step_dt(10, 0.1, f0(0))
-            run_results.append(interest_rates.copy())
-            M_matrix.append([1])
+            interest_rates = ho_lee.eval_time_T_step_dt(10, 0.1, f0(0))
+            run_results_retval.append(interest_rates.copy())
+            M_matrix_retval.append([1])
             for rr in range(len(interest_rates.copy()[1:])):
-                last_value = M_matrix[-1][-1]
-                M_matrix[-1].append(
+                last_value = M_matrix_retval[-1][-1]
+                M_matrix_retval[-1].append(
                     last_value * math.exp((interest_rates.copy()[rr + 1] + interest_rates.copy()[rr]) * 0.5 * .1)
                 )
+        return run_results_retval, M_matrix_retval
 
-        # average the simulations
-        averaged_results = []
-        P_graph = []
-
+    def average_ho_lee_simulations(self, number_of_runs, time_values, simulation_run_results, simulation_M_matrix):
+        averaged_results_retval = []
+        P_graph_retval = []
         for i in range(len(time_values)):
             s = 0
             n = 0
             for j in range(number_of_runs):
-                s += run_results[j][i]
+                s += simulation_run_results[j][i]
                 n += 1
-            averaged_results.append(1.0 * s / n)
+            averaged_results_retval.append(1.0 * s / n)
             ms = 0
             mn = 0
             for j in range(number_of_runs):
-                ms += 1.0 / M_matrix[j][i]
+                ms += 1.0 / simulation_M_matrix[j][i]
                 mn += 1
-            P_graph.append(1.0 * ms / mn)
+            P_graph_retval.append(1.0 * ms / mn)
+        return averaged_results_retval, P_graph_retval
 
-        # plot average of simulations and show you reconstruct original P(t,T) discount curve
+    def plot_graph(self, time_values, P_graph):
         plt.plot(time_values, P_graph, 'o-')
-        plt.plot(time_values, [math.exp(-0.04 * ti / 10) for ti in range(0, 100)], 'o-')
+        plt.plot(time_values, [
+            math.exp(-self.interest_rate_curve_parameters[self.interest_rate_curve]["parameters"][0] * ti * self.dt) for
+            ti in range(0, 100)], 'o-')
         plt.legend("Original", "Ho-Lee Approximation")
         plt.show()
+
+    def graph_exponential_example(self) -> None:
+        if self.interest_rate_curve == "exponential":
+            P = lambda t: math.exp(-self.interest_rate_curve_parameters[self.interest_rate_curve]["parameters"][0] * t)
+            f0 = lambda t: -(math.log(P(t + self.dt)) - math.log(P(t))) / self.dt
+            frf = lambda T: (f0(T + self.dt) - f0(T - self.dt)) / (2 * self.dt) + self.sigma * self.sigma * T
+            theta = lambda x: frf
+            hl = ho_lee.HoLee(theta, self.sigma)
+
+            # do a bunch of Ho-Lee simulations
+            number_of_runs = 1000
+            run_results, M_matrix = self.run_ho_lee_simulations(number_of_runs, hl, f0)
+
+            # average the simulations
+            time_values = [self.dt * i for i in range(1, 101)]
+            averaged_results, P_graph = self.average_ho_lee_simulations(number_of_runs, time_values, run_results, M_matrix)
+
+            # plot average of simulations and show you reconstruct original P(t,T) discount curve
+            self.plot_graph(time_values, P_graph)
 
     def get_interest_rate_curve(self):
         nss = nelson_siegel_svensson.NelsonSiegelSvensson()
